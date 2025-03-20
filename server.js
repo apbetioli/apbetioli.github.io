8bffa570a8e88bfdcc88418da08915670d49c9fc
Alexandre Betioli <apbetioli@gmail.com>
2025-03-20 21:50:27 +0100

    FIx server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.resolve(__dirname, 'src')));
 
 // Needed for the client-side routing to work when refreshing the page
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'src', 'index.html'));
 });
 
 app.listen(port, () => {
