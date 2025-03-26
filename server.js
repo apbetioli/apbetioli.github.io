a40662983d70bbd81057220b55da4e762c4e7ec1
Alexandre Betioli <apbetioli@gmail.com>
2025-03-26 18:04:27 +0100

    Serve app with node.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.static(path.resolve(__dirname, 'src')));
 
